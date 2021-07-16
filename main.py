f89b9fa8aebac1e5395996fd1a5776f60b5acc9a
AndyHide <andyhidesds@gmail.com>
2021-07-16 14:15:08 +0500

    added print_field function

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-table = [['-' for x in range(3)] for x in range(3)]
+field = [['-' for x in range(3)] for x in range(3)]
 
-pass
-pass
+def print_field():
+    print('  1 2 3')
+    print(f'a {field[0][0]} {field[0][1]} {field[0][2]}')
+    print(f'b {field[1][0]} {field[1][1]} {field[1][2]}')
+    print(f'c {field[2][0]} {field[2][1]} {field[2][2]}')
